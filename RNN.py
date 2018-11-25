4013270d7f5551793c90e473dbfe15f7ddfc0dbd
Alex <1668926294@qq.com>
2018-11-25 14:45:45 +0800

    逻辑回归，数据采集的选择

--- RNN.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-import matplotlib.pylab as plt
-from tensorflow.examples.tutorials.mnist import input_data
-import pandas as pd
-import os
-import glob
-

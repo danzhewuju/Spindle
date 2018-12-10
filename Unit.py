11883be61d9716aab91b6bb8aa9381830eb63c57
Alex <1668926294@qq.com>
2018-12-10 13:26:12 +0800

    字符串的相似性

--- Unit.py
+++ Unit.py
@@ -4,6 +4,7 @@ import glob
 import pandas as pd
 import numpy as np
 import keras.preprocessing as preprocessing
+import Levenshtein
 
 
 class SpindleData:
@@ -11,16 +12,18 @@ class SpindleData:
     paths = []
     labels = []
     data = []
-    step = 0.0001
-    max_length = 0#设置默认的编码间隔
+    length = 0 #固定长度的设置
+    step = 0.0001#设置默认的编码间隔
+    max_length = 0 #序列的最大长度
+    mean_length = 0 #序列的平均长度
     coding_w = []  #元素的数据编码
-    coding_q = []
+    coding_q = []  #将序列弄成相同的维度
 
     def __init__(self, path="datasets", step=0.0001 ):
         self.path = path
         self.step =step
         self.paths, self.labels = self.get_data_labels()   #获得路径以及标签
-        self.coding()
+        self.coding()                      #
 
     def get_data_labels(self):  # 返回获取的数据以及标签[0,1,0,1,...]  "./datasets/"
         path = self.path
@@ -47,11 +50,13 @@ class SpindleData:
             print("正在对第%d个序列进行编码..."%(i+1))
             codeing_q.append(code)#将二位的编码加入到序列中
         self.max_length = max([len(x) for x in codeing_q])
+        self.mean_length = np.mean(np.asarray([len(x) for x in codeing_q]))
         self.coding_w = codeing_q
-        codeing_q = preprocessing.sequence.pad_sequences(codeing_q, maxlen=self.max_length)   #将所有的串都弄成相同的维度
+        # codeing_q = preprocessing.sequence.pad_sequences(codeing_q, maxlen=self.max_length)   #将所有的串都弄成相同的维度
+        codeing_q = preprocessing.sequence.pad_sequences(codeing_q, maxlen=int(self.mean_length))  # 将所有的串都弄成相同的维度
         self.coding_q = np.asarray(codeing_q)
 
-    def writer_coding(self):
+    def writer_coding(self):                                         #将数据的原始编码写入到文件中（没有对齐的数据）
         f = open("./data/cases_encoding.txt", 'w', encoding="UTF-8")
         fp = open("./data/controls_encoding.txt", 'w', encoding="UTF-8")
         cate =[x for x in os.listdir("datasets/cases")]
@@ -70,8 +75,36 @@ class SpindleData:
         fp.close()
         print("Writing Success!!!")
 
+    @classmethod
+    def trans_list_str(self, list_a):#将数组转化为字符串
+        str_a = ""
+        for a in list_a:
+            str_a += str(a)
+        return str_a
 
-def bit_coding(data, step): #对一个数据进行编码
+    def writing_coding_str(self):  #将对齐编码转化为字符串的形式
+        f = open("./data/cases_encoding_str.txt", 'w', encoding="UTF-8")
+        fp = open("./data/controls_encoding_str.txt", 'w', encoding="UTF-8")
+        cate = [x for x in os.listdir("datasets/cases")]
+        n = cate.__len__()
+        for index, p in enumerate(self.coding_q):
+            name = self.paths[index].split('\\')[-1]
+            if index < n:
+                f.write(name + ":")
+                str_a = SpindleData.trans_list_str(p)
+                f.writelines(str_a)
+                f.write("\n")
+            else:
+                fp.write(name + ":")
+                str_a = SpindleData.trans_list_str(p)
+                fp.writelines(str_a)
+                fp.write("\n")
+        f.close()
+        fp.close()
+        print("Writing Success!!!")
+
+
+def bit_coding(data, step): #对一个数据进行二进制编码的实现方法
     code = []
     pre_data = 0
     count = 0
@@ -132,11 +165,29 @@ def get_all_data(paths):
 
 
 def test(): #这里是测试方法
-    path = "datasets"
-    spindle = SpindleData(step=0.001)
-    spindle.writer_coding()   #获得二进制编码
-
+    # path = "datasets"
+    # spindle = SpindleData(step=0.001)
+    # print("mean:%f" % spindle.mean_length)
+    # spindle.writing_coding_str()
+    calculate_distance()
     return True
+
+
+def calculate_distance():
+    f = open("data/cases_encoding_str.txt", 'r', encoding="UTF-8")
+    data_cases = []
+    for line in f:
+        data_cases.append(line.split(":")[-1])
+    print("cases_encoding_str文件读取完成！")
+    f.close()
+    data_controls = []
+    f = open("data/controls_encoding_str.txt", 'r', encoding="UTF-8")
+    for line in f:
+        data_controls.append(line.split(":")[-1])
+    print("controls_encoding_str文件读取完成！")
+    f.close()
+    for d in data_cases:
+        print(Levenshtein.jaro(data_cases[1], d))
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -7,23 +7,35 @@ import pandas as pd
 import os
 import glob
 from Unit import bit_coding
-from lstm_sequence import SpindleData
-path = "datasets"
-spindle = SpindleData()
-datas = spindle.data
-# print(spindle.data.__len__())
-data_mean = []
-for d in datas:
-    # d = d['Time_of_night']
-    data_mean.append((d[len(d)-1]-d[0])/len(d))
-
-print(data_mean)
-print(np.mean(data_mean)*3600)
+import Levenshtein
+# from lstm_sequence import SpindleData
+# path = "datasets"
+# spindle = SpindleData()
+# datas = spindle.data
+# # print(spindle.data.__len__())
+# data_mean = []
+# for d in datas:
+#     # d = d['Time_of_night']
+#     data_mean.append((d[len(d)-1]-d[0])/len(d))
+#
+# print(data_mean)
+# print(np.mean(data_mean)*3600)
 
 # print(data)
 # code =bit_coding(data, step=0.0001)
 # # code = np.asarray(code)
 # print(code)
+f = open("data/cases_encoding_str.txt", 'r', encoding="UTF-8")
+data_cases = []
+for line in f:
+    data_cases.append(line.split(":")[-1])
+f.close()
+data_controls = []
+f = open("data/controls_encoding_str.txt", 'r', encoding="UTF-8")
+for line in f:
+    data_controls.append(line.split(":")[-1])
+f.close()
+print(Levenshtein.jaro(data_controls[1], data_cases[2]))
 
 
 
